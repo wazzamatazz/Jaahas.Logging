1b2b43f230390298efe6ba38aa7e6149c46512e7
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-17 16:19:23 +0300

    Update docs and readme

--- src/Jaahas.Extensions.Logging.CommonLogging/CommonLoggingLoggerProvider.cs
+++ src/Jaahas.Extensions.Logging.CommonLogging/CommonLoggingLoggerProvider.cs
@@ -1,25 +1,50 @@
 ﻿using Microsoft.Extensions.Logging;
 
 namespace Jaahas.Extensions.Logging.CommonLogging {
+
+    /// <summary>
+    /// <see cref="ILoggerProvider"/> implementation that creates <see cref="CommonLoggingLogger"/> 
+    /// instances.
+    /// </summary>
     public class CommonLoggingLoggerProvider : ILoggerProvider {
 
+        /// <summary>
+        /// The Common.Logging log manager.
+        /// </summary>
         private readonly Common.Logging.ILogManager _logManager;
 
+        /// <summary>
+        /// The external scope provider for the logger provider.
+        /// </summary>
         private readonly IExternalScopeProvider _scopeProvider;
 
 
+        /// <summary>
+        /// Creates a new <see cref="CommonLoggingLoggerProvider"/> instance.
+        /// </summary>
+        /// <param name="logManager">
+        ///   The Common.Logging log manager to use. Specify <see langword="null"/> to use a new 
+        ///   <see cref="Common.Logging.LogManager"/> instance.
+        /// </param>
+        /// <param name="scopeProvider">
+        ///   The <see cref="IExternalScopeProvider"/> to use.
+        /// </param>
         public CommonLoggingLoggerProvider(Common.Logging.ILogManager logManager, IExternalScopeProvider scopeProvider = null) {
             _logManager = logManager ?? new Common.Logging.LogManager();
             _scopeProvider = scopeProvider;
         }
 
 
+        /// <inheritdoc/>
         public ILogger CreateLogger(string categoryName) {
             return new CommonLoggingLogger(_logManager.GetLogger(categoryName), _scopeProvider);
         }
 
+
+        /// <inheritdoc/>
         public void Dispose() {
             // Do nothing.
         }
+
     }
 }
--- src/Jaahas.Extensions.Logging.CommonLogging/README.md
+++ src/Jaahas.Extensions.Logging.CommonLogging/README.md
@@ -1,5 +1,21 @@
-﻿# Jaahas.Logging.Extensions.CommonLogging
+﻿# About
 
-[Microsoft.Extensions.Logging](https://github.com/aspnet/Extensions/tree/master/src/Logging) implementation that logs to [Common.Logging](https://github.com/net-commons/common-logging).
+Jaahas.Extensions.Logging.CommonLogging is a `Microsoft.Extensions.Logging.ILoggerProvider` implementation that writes log messages to [Common.Logging](https://github.com/net-commons/common-logging).
 
-This allows you to integrate libraries that use `Microsoft.Extensions.Logging` into existing applications that use `Common.Logging`.
+This allows libraries and components that write log messages using modern .NET logging infrastructure to be consumed by legacy applications that use Common.Logging without requiring a rewrite of logging code.
+
+
+# Getting Started
+
+Add the Common.Logging provider to your logger configuration when building your dependency injection container:
+
+```csharp
+services.AddLogging(logging => {
+    // Remove the default logging providers and add Common.Logging instead
+    logging.ClearProviders().AddCommonLogging();
+});
+```
+
+Refer to the [Common.Logging documentation](https://github.com/net-commons/common-logging) for information on how to configure Common.Logging to log to your destination(s) of choice.
+
+
